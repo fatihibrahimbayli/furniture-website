a257cbc47b5472af43394ab530eea2cbb5ce853c
fatihibrahimbayli <fatihgi@code.edu.az>
2026-03-27 12:46:30 +0400

    Models created

---
+++ FurnitureApp/Services/Interfaces/ISliderService.cs
@@ -0,0 +1,13 @@
+﻿using FurnitureApp.Models;
+
+namespace FurnitureApp.Services.Interfaces
+{
+    public interface ISliderService
+    {
+        Task<List<Slider>> GetAllAsync();
+         Task<Slider> GetByIdAsync(int id);
+         Task CreateAsync(Slider slider);
+         Task UpdateAsync(Slider slider);
+         Task DeleteAsync(int id);
+    }
+}
---
+++ FurnitureApp/Services/SliderService.cs
@@ -0,0 +1,49 @@
+﻿using FurnitureApp.Data;
+using FurnitureApp.Models;
+using FurnitureApp.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureApp.Services
+{
+    public class SliderService : ISliderService
+    {
+        private readonly AppDbContext _dbContext;
+        public SliderService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task CreateAsync(Slider slider)
+        {
+            await _dbContext.Sliders.AddAsync(slider);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+           var slider= await _dbContext.Sliders.FindAsync(id);
+           if(slider is null)
+            {
+                throw new Exception("Slider not found");
+            }
+             _dbContext.Sliders.Remove(slider);
+            await _dbContext.SaveChangesAsync();
+
+
+        }
+
+        public async Task<List<Slider>> GetAllAsync()
+        {
+           return await _dbContext.Sliders.ToListAsync();
+        }
+
+        public Task<Slider> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Slider slider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
